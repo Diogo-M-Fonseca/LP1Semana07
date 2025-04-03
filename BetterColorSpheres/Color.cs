a0b7c0f5d5f88a8b11f4f51f09de73aa01ccbd00
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 23:09:45 +0100

    created Color class variables

---
+++ BetterColorSpheres/Color.cs
@@ -0,0 +1,10 @@
+namespace BetterColorSpheres
+{
+    public class Color
+    {
+        private readonly byte _red;
+        private readonly byte _green;
+        private readonly byte _blue;
+        private byte _alpha;
+    }
+}
