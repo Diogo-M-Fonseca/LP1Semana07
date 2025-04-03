92c250b566110925ece6e3066844bd324cd13c44
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 23:18:11 +0100

    added Sphere variables

---
+++ BetterColorSpheres/Sphere.cs
@@ -0,0 +1,9 @@
+namespace BetterColorSpheres   
+{
+    public class Sphere
+    {
+        private readonly Color _color;
+        private float _radius;
+        private int _timesThrown;
+    }
+}
