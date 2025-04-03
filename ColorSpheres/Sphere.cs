44574d5890ad44e0c9f020674a48121d6bf47f7e
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 22:35:16 +0100

    contructor Sphere created

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -5,5 +5,12 @@ namespace ColorSpheres
         private Color _color;
         private float _radius;
         private int _timesThrown;
+
+        public Sphere(Color color, float radius)
+        {
+            _color = color;
+            _radius = radius;
+            _timesThrown = 0;
+        }
     }
 }
