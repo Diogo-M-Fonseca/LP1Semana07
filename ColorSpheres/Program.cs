2f646344eeb2c4b9bcee1cf0357c4ee2aab3be1e
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 22:48:58 +0100

    created more colors and spheres and used Throw() and Pop()

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -7,7 +7,30 @@ namespace ColorSpheres
         private static void Main(string[] args)
         {
             Color color1 = new Color(255,50,50);
-            Sphere sphere = new Sphere(color1,3);
+            Color color2 = new Color(50,50,50);
+            Color color3 = new Color(55,5,150);
+            Color color4 = new Color(10,255,50);
+
+            Sphere sphere = new Sphere(color1,5);
+            Sphere sphere2 = new Sphere(color2,3);
+            Sphere sphere3 = new Sphere(color3,2);
+            Sphere sphere4 = new Sphere(color4,10);
+
+
+            sphere2.Throw();
+            sphere3.Pop();
+            sphere4.Throw();
+            sphere.Throw();
+            sphere2.Pop();
+            sphere3.Throw();
+            sphere4.Throw();
+            sphere.Pop();
+            sphere2.Throw();
+            sphere3.Throw();
+            sphere4.Pop();
+            sphere.Throw();
+            
+
         }
     }
 }
