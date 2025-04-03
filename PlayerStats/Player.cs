e9c5c1168755d455c31b3af5c79e066690f49646
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 23:49:35 +0100

    HighScore propertys

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -5,5 +5,20 @@ namespace PlayerStats
         private float _highScore;
         private int _playedGames;
         private int _wonGames;
+
+        public float HighScore
+        {
+            get
+            {
+                return _highScore;
+            }
+            set
+            {
+                if (HighScore > _highScore)
+                {
+                    _highScore = HighScore;
+                }
+            }
+        }
     }
 }
