6fb1917623ee4d2c376d995b81378be95ec0efe9
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 23:10:52 +0100

    added old constructors

--- BetterColorSpheres/Color.cs
+++ BetterColorSpheres/Color.cs
@@ -6,5 +6,22 @@ namespace BetterColorSpheres
         private readonly byte _green;
         private readonly byte _blue;
         private byte _alpha;
+
+         public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            _red = red;
+            _green = green;
+            _blue = blue;
+            _alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            _red = red;
+            _green = green;
+            _blue = blue;
+            _alpha = 255;
+        }
+
     }
 }
