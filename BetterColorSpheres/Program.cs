b36f9c989cc419ceb0096cecd5781f9f3e2c5df1
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-03 23:25:30 +0100

    fixed method propertys problem and changed main

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
-
-namespace BetterColorSpheres
+﻿namespace BetterColorSpheres
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Color sphereColor = new Color(125, 244, 255);
+            Sphere sphere = new Sphere(sphereColor, 5);
+
+            sphere.Throw();
+            sphere.Throw();
+            sphere.Throw();
+            sphere.Pop();
+
+            System.Console.WriteLine($"The sphere has bounced {sphere.GetTimesThrown()} times,its color is {sphereColor.GetGrey()}");
         }
+
     }
 }
