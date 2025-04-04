0de9e6bc644952ee18b2271139ff8e2eacb13435
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-04 17:19:39 +0100

    MaxHealth property created

--- MyRoguelike/Hero.cs
+++ MyRoguelike/Hero.cs
@@ -4,7 +4,6 @@ namespace MyRoguelike
     {
         private int xp;
         private int health;
-        private float maxHealth;
         private string Name {get;}
 
         public int XP
@@ -30,11 +29,16 @@ namespace MyRoguelike
                 {
                     health = 0;
                 }
-                else if (health > maxHealth)
+                else if (health > MaxHealth)
                 {
-                    health = (int)maxHealth;
+                    health = (int)MaxHealth; // pode haver problemas com a conv
                 }
             }
+        }
+
+        public float MaxHealth 
+        {
+            get => 100 + (Level - 1) * 20;
         }
 
 
